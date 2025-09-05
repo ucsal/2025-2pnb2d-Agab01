c30ac496aa103cc6a4e6c501391b7eddd412ea75
agab0 <agab0@DESKTOP-7NAR3N9>
2025-09-04 23:58:24 -0300

    Final commit

--- src/main/java/br/com/mariojp/solid/dip/EmailNotifier.java
+++ src/main/java/br/com/mariojp/solid/dip/EmailNotifier.java
@@ -1,12 +1,18 @@
 package br.com.mariojp.solid.dip;
 
 public class EmailNotifier {
-	private final SmtpClient smtp = new SmtpClient();
 
-	public void welcome(User user) {
-		if ("true".equalsIgnoreCase(System.getProperty("DRY_RUN"))) {
-			// Estado inicial: ainda assim usa SMTP real (bug proposital)
-		}
-		smtp.send(user.email(), "Bem-vindo", "Olá " + user.name());
-	}
+    private final MailSender mailSender;
+
+    public EmailNotifier() {
+        if ("true".equalsIgnoreCase(System.getProperty("DRY_RUN"))) {
+            this.mailSender = new NoopMailSender();
+        } else {
+            this.mailSender = new SmtpMailSender();
+        }
+    }
+
+    public void welcome(User user) {
+        mailSender.send(user.email(), "Bem-vindo", "Olá " + user.name());
+    }
 }
